0c8b91334f939be17c5ff4436dd9f22a4b2529ec
Janardhan Babu Pati <156563226+janardhanbabu666@users.noreply.github.com>
2025-09-11 10:42:16 +0530

    Delete backend directory

--- backend/routes/transactionRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { addExpense, getExpenses } from "../controllers/expenseController.js";
-
-const router = express.Router();
-
-router.post("/", addExpense);        // Add expense
-router.get("/:userId", getExpenses); // Get all expenses for a user
-
-export default router;
